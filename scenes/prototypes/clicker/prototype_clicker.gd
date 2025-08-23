dfdae542d971bda189b509fce2eb417d514cd129
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2025-08-23 18:43:01 +0100

    Add label, button, create stardust, update label

---
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -0,0 +1,25 @@
+class_name PrototypeClicker
+extends Control
+## A clicker prototype creating stardust
+
+## References to the label displaying the current amount of stardust
+@export var label : Label
+## Current amount of stardust created
+var stardust : int = 0
+
+## Initialise the label at launch
+func _ready() -> void:
+	update_label_text()
+
+## Create 1 stardust
+func create_stardust() -> void:
+	stardust += 1
+	
+## Update the label text to reflect current stardust amount
+func update_label_text() -> void:
+	label.text = "Stardust: %s" %stardust
+
+## Create stardust + update the label with the new value
+func _on_button_pressed() -> void:
+	create_stardust()
+	update_label_text()
